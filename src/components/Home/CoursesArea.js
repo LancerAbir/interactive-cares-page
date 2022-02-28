bc431eff98ac7f1b41fe211cacd30b9655338cd5
LancerAbir <anjan.o24@gmail.com>
2022-02-28 11:44:49 +0600

    dsadas

--- src/components/Home/CoursesArea.js
+++ src/components/Home/CoursesArea.js
@@ -40,6 +40,151 @@ const CoursesArea = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="col-md-4">
+                        <div className="course__box">
+                            <img
+                                src="/img/spoken-english-learning-path-cover-768x402.jpg"
+                                alt="Course__Image"
+                            />
+                            <div className="course__content">
+                                <i className="far fa-star"></i>
+                                <h3>Spoken English Learning Path</h3>
+                                <div className="course__by">
+                                    <a className="ic" href="#">
+                                        ic
+                                    </a>
+                                    by
+                                    <a href="#">Interactive Cares</a>
+                                </div>
+                                <div className="course__rate">
+                                    <h4 className="course__rate__delete">
+                                        ৳ 3,000.00
+                                    </h4>
+                                    <h4>৳ 600.00</h4>
+                                    <div className="course__addToCart">
+                                        <i className="fas fa-shopping-cart"></i>
+                                        Add to cart
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="course__box">
+                            <img
+                                src="/img/spoken-english-learning-path-cover-768x402.jpg"
+                                alt="Course__Image"
+                            />
+                            <div className="course__content">
+                                <i className="far fa-star"></i>
+                                <h3>Spoken English Learning Path</h3>
+                                <div className="course__by">
+                                    <a className="ic" href="#">
+                                        ic
+                                    </a>
+                                    by
+                                    <a href="#">Interactive Cares</a>
+                                </div>
+                                <div className="course__rate">
+                                    <h4 className="course__rate__delete">
+                                        ৳ 3,000.00
+                                    </h4>
+                                    <h4>৳ 600.00</h4>
+                                    <div className="course__addToCart">
+                                        <i className="fas fa-shopping-cart"></i>
+                                        Add to cart
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="course__box">
+                            <img
+                                src="/img/spoken-english-learning-path-cover-768x402.jpg"
+                                alt="Course__Image"
+                            />
+                            <div className="course__content">
+                                <i className="far fa-star"></i>
+                                <h3>Spoken English Learning Path</h3>
+                                <div className="course__by">
+                                    <a className="ic" href="#">
+                                        ic
+                                    </a>
+                                    by
+                                    <a href="#">Interactive Cares</a>
+                                </div>
+                                <div className="course__rate">
+                                    <h4 className="course__rate__delete">
+                                        ৳ 3,000.00
+                                    </h4>
+                                    <h4>৳ 600.00</h4>
+                                    <div className="course__addToCart">
+                                        <i className="fas fa-shopping-cart"></i>
+                                        Add to cart
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="course__box">
+                            <img
+                                src="/img/spoken-english-learning-path-cover-768x402.jpg"
+                                alt="Course__Image"
+                            />
+                            <div className="course__content">
+                                <i className="far fa-star"></i>
+                                <h3>Spoken English Learning Path</h3>
+                                <div className="course__by">
+                                    <a className="ic" href="#">
+                                        ic
+                                    </a>
+                                    by
+                                    <a href="#">Interactive Cares</a>
+                                </div>
+                                <div className="course__rate">
+                                    <h4 className="course__rate__delete">
+                                        ৳ 3,000.00
+                                    </h4>
+                                    <h4>৳ 600.00</h4>
+                                    <div className="course__addToCart">
+                                        <i className="fas fa-shopping-cart"></i>
+                                        Add to cart
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="course__box">
+                            <img
+                                src="/img/spoken-english-learning-path-cover-768x402.jpg"
+                                alt="Course__Image"
+                            />
+                            <div className="course__content">
+                                <i className="far fa-star"></i>
+                                <h3>Spoken English Learning Path</h3>
+                                <div className="course__by">
+                                    <a className="ic" href="#">
+                                        ic
+                                    </a>
+                                    by
+                                    <a href="#">Interactive Cares</a>
+                                </div>
+                                <div className="course__rate">
+                                    <h4 className="course__rate__delete">
+                                        ৳ 3,000.00
+                                    </h4>
+                                    <h4>৳ 600.00</h4>
+                                    <div className="course__addToCart">
+                                        <i className="fas fa-shopping-cart"></i>
+                                        Add to cart
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="row">
--- src/components/Home/WebDevelopmentArea.js
+++ src/components/Home/WebDevelopmentArea.js
@@ -1,19 +1,23 @@
 const WebDevelopmentArea = () => {
     return (
         <section className="webDevelopment__area">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 offset-md-6">
-                        <div className="webDevelopment__area__content">
-                            <div className="webDevelopment__area__box">
-                                <h2>ফুল স্ট্যাক ওয়েব ডেভেলপমেন্ট</h2>
-                                <p>
-                                    ওয়েব ডেভেলপার হওয়ার স্বপ্ন এবার হবে পূরণ।
-                                    সম্পূর্ণ ফ্রন্ট ও ব্যাক এন্ড ডেভেলপমেন্ট
-                                    শেখা হবে শূন্য থেকেই
-                                </p>
-                                <div className="webDevelopment__btn d-flex justify-content-end">
-                                    বিস্তারিত <br /> দেখুন
+            <div className="webDevelopment__area__bg">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 offset-md-1">
+                            <div className="webDevelopment__area__content">
+                                <div className="webDevelopment__area__box">
+                                    <h2>ফুল স্ট্যাক ওয়েব ডেভেলপমেন্ট</h2>
+                                    <p>
+                                        ওয়েব ডেভেলপার হওয়ার স্বপ্ন এবার হবে
+                                        পূরণ। সম্পূর্ণ ফ্রন্ট ও ব্যাক এন্ড
+                                        ডেভেলপমেন্ট শেখা হবে শূন্য থেকেই
+                                    </p>
+                                    <div className="webDevelopment__btn d-flex justify-content-end">
+                                        <a href="#">
+                                            বিস্তারিত <br /> দেখুন
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
