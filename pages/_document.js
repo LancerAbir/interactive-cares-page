f0c107bb25a7e0fc93a75a98006af29fa3e2db51
Lancer Abir <anjan.o24@gmail.com>
2022-03-01 20:13:36 +0600

    dad

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,17 @@ export default function Document() {
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
                 />
+
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Archivo:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800&family=Roboto:wght@100;300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                ></link>
             </Head>
             <body>
                 <Main />
